e9621c968d9f37f2a4e98f848c6244a18236716e
Dave Goodman <davegoodman20@gmail.com>
2018-01-31 15:46:59 -0500

    final model tests for productins

--- test/models/production_test.rb
+++ test/models/production_test.rb
@@ -3,13 +3,13 @@ require 'test_helper'
 class ProductionTest < ActiveSupport::TestCase
 
   test 'should not save production without a title' do
-    @production = Production.new
+    @production = Production.new(production_type: "film", budget: 5000, date_begin: 2018-03-03, date_end: 2018-02-02, studio_id: 1, description: "Test descripion.")
 
     assert_not @production.save
   end
 
   test 'should be instance of Production class' do
-    @production = Production.new
+    @production = Production.new(production_type: "film", budget: 5000, date_begin: 2018-03-03, date_end: 2018-02-02, studio_id: 1, description: "Test descripion.")
 
     @production.save
 
